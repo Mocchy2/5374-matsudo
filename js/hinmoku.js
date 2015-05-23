3623439f18746a229c6a505c123d91475c812228
望月　大樹 <Mocchy@users.noreply.github.com>
2015-05-23 15:58:01 +0900

    Update hinmoku.js

--- js/hinmoku.js
+++ js/hinmoku.js
@@ -1,6 +1,15 @@
-            $(function() {
-                        $("#hinmoku").change(function() {
-                            var myValue = $('#hinmoku option:selected').val();
-                            $("#hinmoku2").text(myValue);
-                        });
-            });
+$(function(){
+	//テキストデータの読込
+	$.get("data/target.csv", function(csvData){
+		for(i=0; i<myData.length; i++) {
+			var resultArray = csvData.split("\n")[i].split(",")[1];
+			var resultArray2 = csvData.split("\n")[i].split(",")[0];
+			$("#hinmoku").append('<option value="'+resultArray2+'">' + resultArray);
+
+                                    $("#hinmoku").change(function() {
+                                                var myValue = $("#kekka option:selected").val();
+                                                $("#hinmoku2").css('color', 'red').text(myValue);
+                                    });
+		}
+	});
+});
