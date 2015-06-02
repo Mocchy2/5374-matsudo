4735516344ab3d6da4df5c52033818a445fa891b
望月　大樹 <Mocchy@users.noreply.github.com>
2015-06-03 00:45:10 +0900

    Update hinmoku.js

--- js/hinmoku.js
+++ js/hinmoku.js
@@ -1,28 +1,37 @@
-$(function(){
-	//csvデータの読込
-	$.get("data/target.csv", function(csvData){
-		var arr1 = [];
-//		var arr2 = [];
-		
-		for(var i=0; i<10; i++) {
-			var row1 = csvData.split("\n")[i].split(",")[0];
-//			var row2 = csvData.split("\n")[i].split(",")[1];
-			arr1.push(row1);
-//			arr2.push(row2);
-			alert(arr1[i]);
-		}
+	$(function() {
+		//テキストデータの読込
+		$.get("data/target.csv", function(csvData) {
+			csvData = csvData.replace("/\r/gm", "");
+			var line = csvData.split("\n"),
+				ret1 = [],
+				ret2 = [],
+				s = "<select>";
 
-//		alert(arr2[7]);
-/*		
-		var s = "<select>";
-		for(var j in csvData) {
-			s += "<option value='"+arr1[j]+"'>"+arr2[j];
-		}
-		s += "</select>";
-		$("#hinmoku")[0].html(s);
+			for (var i in line) {
+				if(line[i].length == 0) continue;
+
+				var row1 = line[i].split(",")[0];
+				var row2 = line[i].split(",")[1];
+				ret1.push(row1);
+				ret2.push(row2);
+			}
+//			document.write(ret, "<br/>");
+
+			for (var j in ret1) {
+				s += "<option value='"+ret2[j]+"'>"+ret1[j];
+			}
+			s += "</select>"
+//			document.write(s, "<br/>");
+//			document.getElementById("hinmoku").innerHTML = s;
+			$("#hinmoku").html(s);
+
+			$("#hinmoku").change(function() {
+				$("#hinmoku2").text($("#hinmoku option:selected").val());
+			});
+
+		});
+
 	});
-});
-*/
 /*
 $(function(){
 	//csvデータの読込
