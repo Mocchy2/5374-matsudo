70f54957e6f57eb58141d72a1210ae9729d5e794
望月　大樹 <Mocchy@users.noreply.github.com>
2015-06-02 11:11:06 +0900

    Update hinmoku.js

--- js/hinmoku.js
+++ js/hinmoku.js
@@ -1,6 +1,29 @@
 $(function(){
 	//csvデータの読込
 	$.get("data/target.csv", function(csvData){
+		var arr1 = [];
+		var arr2 = [];
+		
+		for(var i in csvData) {
+			var row1 = csvData.split("\n")[i].split(",")[0];
+			var row2 = csvData.split("\n")[i].split(",")[1];
+			arr1.push(row1);
+			arr2.push(row2);
+		}
+		
+		var s = "<select>";
+		for(var j in csvData) {
+			s += "<option value='"+arr1[j]+"'>"+arr2[j];
+		}
+		s += "</select>";
+//		$("#hinmoku")
+	});
+});
+
+/*
+$(function(){
+	//csvデータの読込
+	$.get("data/target.csv", function(csvData){
 		for(i=1; i<csvData.length; i++) {
 			var row = csvData.split("\n")[i].split(",")[0];
 			var row2 = csvData.split("\n")[i].split(",")[1];
@@ -11,3 +34,4 @@ $(function(){
 		}
 	});
 });
+*/
