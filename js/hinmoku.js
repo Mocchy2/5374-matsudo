ce3de54b88990f7ec578c486239dd8a242f51e67
望月　大樹 <Mocchy@users.noreply.github.com>
2015-06-03 13:57:09 +0900

    Update hinmoku.js

--- js/hinmoku.js
+++ js/hinmoku.js
@@ -3,47 +3,30 @@
 		$.get("data/target.csv", function(csvData) {
 			csvData = csvData.replace("/\r/gm", "");
 			var line = csvData.split("\n"),
-				ret1 = [],
-				ret2 = [],
+				ret1 = [], //ごみの種類 配列
+				ret2 = [], //ごみ品目　 配列
 				s = "<select>";
 				
 			s += "<option value=''>ごみの種類を選択してください";
 
 			for (var i=1; i<line.length; i++) {
-				if(line[i].length == 0) continue;
+				if(line[i].length == 0) continue; //空行はスキップ
 
 				var row1 = line[i].split(",")[0];
 				var row2 = line[i].split(",")[1];
 				ret1.push(row1);
 				ret2.push(row2);
 			}
-//			document.write(ret, "<br/>");
-			
+
 			for (var j in ret1) {
-				s += "<option value='"+ret1[j]+"'>"+ret2[j];
+				s += "<option value='"+ret1[j]+"'>"+ret2[j]; //セレクトボックス作成
 			}
 			s += "</select>"
-//			document.write(s, "<br/>");
-//			document.getElementById("hinmoku").innerHTML = s;
-			$("#hinmoku").html(s).change(function() {
-				$("#hinmoku2").text($("#hinmoku option:selected").val());
+
+			$("#hinmoku").html(s).change(function() { //表示
+				$("#hinmoku2").text($("#hinmoku option:selected").val()); 
 			});
 			
 		});
 		
 	});
-/*
-$(function(){
-	//csvデータの読込
-	$.get("data/target.csv", function(csvData){
-		for(i=1; i<csvData.length; i++) {
-			var row = csvData.split("\n")[i].split(",")[0];
-			var row2 = csvData.split("\n")[i].split(",")[1];
-			$("#hinmoku").append('<option value="'+row+'">' + row2)
-				.change(function() {
-					$("#hinmoku2").text($("#hinmoku option:selected").val());
-				});
-		}
-	});
-});
-*/
